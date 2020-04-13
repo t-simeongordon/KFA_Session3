8975f6d788bca7376ab291d0750145f34601a165
t-simeon-gordon <terrel.simeon-gordon@accenture.com>
2020-04-13 04:15:21 +0100

    added ui button

--- app/src/main/java/com/tsg/kfa_session3/MainActivity.kt
+++ app/src/main/java/com/tsg/kfa_session3/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 
 // FIRST enable version control integration -> VCS -> enable version control integration -> select git -> select okay
 // 2. select project dirctory path view instead of android -> navigate to .gitignore -> google gitignore.io -> search required environments -> copy/paste generated
-// 3. Share project -> VCS -> Import into version control -> share project on GIT
+// 3. Share project -> VCS -> Import into version control -> share project on GIT -> login -> add
+// 4. create new branch -> VCS -> Git -> branches -> create new branch -> ok
+// 5. commit changes -> add ui button -> select commit changes ->
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
