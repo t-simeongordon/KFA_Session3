70fb592701e5e31e913a711d2058ee1d96b485db
t-simeon-gordon <terrel.simeon-gordon@accenture.com>
2020-04-13 03:27:14 +0100

    Initial commit

---
+++ app/src/main/java/com/tsg/kfa_session3/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.tsg.kfa_session3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// FIRST enable version control integration -> VCS -> enable version control integration -> select git -> select okay
+// 2. select project dirctory path view instead of android -> navigate to .gitignore -> google gitignore.io -> search required environments -> copy/paste generated
+// 3. Share project -> VCS -> Import into version control -> share project on GIT
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
